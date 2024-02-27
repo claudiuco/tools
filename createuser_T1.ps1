7bd6ae62e9507d21fe119916ae0187c09b14a97e
The Good Root <personal@claunix.com>
2024-02-27 13:05:16 +0100

    Create createuser_T1.ps1

---
+++ createuser_T1.ps1
@@ -0,0 +1,45 @@
+## Title:       Automate AD user creation (T1 accounts in Microsoft Tier Model. 
+## Author:      Claudiu Costin
+## Version:     1.5
+## Date:        2024-02-27
+
+#Change to correct directory
+Set-Location -Path "C:\Temp\ADAccounts"
+
+#Enter a path to your import CSV file
+$ADUsers = Import-csv .\T1_NewServiceAccounts.csv -Delimiter ","
+
+foreach ($User in $ADUsers)
+{
+
+       $Username    = $User.username
+       $Password    = $User.password
+       $Firstname   = $User.firstname
+       $OU          = $User.ou
+       $Description = $user.description
+
+       #Check if the user account already exists in AD
+       if (Get-ADUser -F {SamAccountName -eq $Username})
+       {
+               #If user does exist, output a warning message
+               Write-Warning "A user account $Username has already exist in Active Directory."
+       }
+       else
+       {
+        #If a user does not exist then create a new user account
+        #Account will be created in the OU listed in the $OU variable in the CSV file; don’t forget to change the domain name in the"-UserPrincipalName" variable
+            New-ADUser `
+            -SamAccountName $Username `
+            -UserPrincipalName "$Username@myfavdomain.com" `
+            -Name "$Firstname" `
+            -Enabled $True `
+            -DisplayName "$Firstname" `
+            -Path $OU `
+            -PasswordNeverExpires $true `
+            -Description $Description `
+            -AccountPassword (convertto-securestring $Password -AsPlainText -Force)
+
+            Write-Warning "User $username processed."
+
+       }
+}
